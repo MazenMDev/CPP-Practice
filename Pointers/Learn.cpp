4f804248739d5980ee97ba58ca166db7baf4456a
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-02-26 00:47:44 +0200

    test the pointers

--- Pointers/Learn.cpp
+++ Pointers/Learn.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 int main(){
@@ -9,3 +10,22 @@ int main(){
 	cout << &w << endl;
 	cout << *w << endl;
 }
+=======
+#include <iostream>
+using namespace std;
+int main(){
+	int y;
+	y = 10;
+	int *w;
+	w = &y;
+	cout << w << endl;
+	cout << &w << endl;
+	cout << *w << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl << endl;
+	cout << y;
+}
+>>>>>>> 89b8871 (test the pointers)
